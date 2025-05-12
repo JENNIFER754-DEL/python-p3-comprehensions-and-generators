f3b1ecf1d3ce098228209f18bcaefc695ca1a74f
JENNIFER754-DEL <jennifernyaguthii23@gmail.com>
2025-05-12 11:01:29 +0300

    Fix list comprehension exercises

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# lib/list_comprehension.py
 
-def make_exclamation(sentence_list):
-    pass
+def return_evens(sequence):
+    """Return a list of all even elements from the given sequence."""
+    return [x for x in sequence if x % 2 == 0]
+
+def make_exclamation(sentences):
+    """Return a list of sentences with exclamation marks at the end."""
+    return [sentence + "!" for sentence in sentences]
